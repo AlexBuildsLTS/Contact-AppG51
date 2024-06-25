755a8507bead78bb864676b624afc86490537948
FullStackOS <alex.youssef@live.com>
2024-06-25 13:31:34 +0200

    Added the fields name mobilenumber and email

--- src/main/java/fullstackOS/org/Contact.java
+++ src/main/java/fullstackOS/org/Contact.java
@@ -1,4 +1,47 @@
-package fullstackOS.org;
+package com.yourname.contactlist;
 
 public class Contact {
+    private String name;
+    private String mobileNumber;
+    private String email;
+
+    public Contact(String name, String mobileNumber, String email) {
+        this.name = name;
+        this.mobileNumber = mobileNumber;
+        this.email = email;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "name='" + name + '\'' +
+                ", mobileNumber='" + mobileNumber + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
