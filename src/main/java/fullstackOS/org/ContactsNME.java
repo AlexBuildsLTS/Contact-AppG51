51bf5468389425a7d04843d59de1052703312891
FullStackOS <alex.youssef@live.com>
2024-06-25 13:50:13 +0200

    Find contacts

--- src/main/java/fullstackOS/org/ContactsNME.java
+++ src/main/java/fullstackOS/org/ContactsNME.java
@@ -1,4 +1,4 @@
-package FullstackOS;
+package fullstackOS.org;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,5 +23,16 @@ public boolean addContact(Contact contact) {
     public List<Contact> getAllContacts() {
         return new ArrayList<>(contacts);
     }
+
+    public List<Contact> findContactsByName(String name) {
+        List<Contact> result = new ArrayList<>();
+        for (Contact c : contacts) {
+            if (c.getName().equalsIgnoreCase(name)) {
+                result.add(c);
+            }
+        }
+        return result;
+    }
+
 
 }
