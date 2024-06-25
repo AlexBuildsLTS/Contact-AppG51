17865bac9bfb8c49602cdf95ea659f3b45f51235
FullStackOS <alex.youssef@live.com>
2024-06-25 13:33:18 +0200

    contactlists updated

--- src/main/java/fullstackOS/org/ContactsNME.java
+++ src/main/java/fullstackOS/org/ContactsNME.java
@@ -1,4 +1,12 @@
-package fullstackOS.org;
+package com.yourname.contactlist;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContactsNME {
+    private List<Contact> contacts;
+
+    public ContactsNME() {
+        this.contacts = new ArrayList<>();
+    }
 }
